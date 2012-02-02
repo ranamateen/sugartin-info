f29698e34b904021b3eabf0f874ca188626705e5
sag333ar@gmail.com <sag333ar@gmail.com@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-02-02 08:26:42 +0000

    AirPrint - Sample Code Ready
    Find the complete illustration here - http://wp.me/p1NbdZ-wG

--- trunk/AirPrintSample/AirPrintSample/ViewController.h
+++ trunk/AirPrintSample/AirPrintSample/ViewController.h
@@ -8,14 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+// add the appropriate delegate 
 @interface ViewController : UIViewController <UIPrintInteractionControllerDelegate> {
+    // have an instance varialbe for accessing printInteraction
     UIPrintInteractionController *printController;
 }
 
+// an imageView from which we will obtain data of an image
 @property (nonatomic, strong) IBOutlet UIImageView *imgV;
+
+// a barButton from which - popOver will Appear
 @property (nonatomic, strong) IBOutlet UIBarButtonItem *item;
 
+// an action which will get triggerd by tapping on BarButton
 -(IBAction)btnPrintTapped:(id)sender;
+
+// a general method which will print the data supplied
 -(void)printItem :(NSData*)data ;
 
 @end
