f833f44e114f4af5e0b9bf2c116b2dc7e36548ff
sag333ar@gmail.com <sag333ar@gmail.com@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-02-03 11:33:58 +0000

    Sample Code ready for the Following functionalities - Show Map - Show WebSite - Make a phone call - Email

---
+++ trunk/ContactUs_Sample/ContactUs_Sample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ContactUs_Sample
+//
+//  Created by Neon Spark on 2/2/12.
+//  Copyright (c) 2012 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
