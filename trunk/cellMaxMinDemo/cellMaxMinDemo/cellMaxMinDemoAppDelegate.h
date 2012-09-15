0ba77b046bf861f0b0cca626922617c97c494f99
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-15 08:22:40 +0000

    Creating MultiLevel UITableView – Expandable & Collapsable UITableView
     See complete illustration here.
     
     http://sugartin.info/2011/07/20/447/

---
+++ trunk/cellMaxMinDemo/cellMaxMinDemo/cellMaxMinDemoAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  cellMaxMinDemoAppDelegate.h
+//  cellMaxMinDemo
+//
+//  Created by Sagar Kothari on 19/07/11.
+//  Copyright 2011 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cellMaxMinDemoAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
