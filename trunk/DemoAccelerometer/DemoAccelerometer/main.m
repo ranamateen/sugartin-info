f2fdd8b83229cabe9265e75cadb8a9bdf4865552
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-18 08:48:21 +0000

    iPhone - enabling Accelerometer usage
    grab complete tutorial from here.
    
    http://sugartin.info/2011/08/11/iphone-enabling-accelerometer-usage/

---
+++ trunk/DemoAccelerometer/DemoAccelerometer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoAccelerometer
+//
+//  Created by Sagar R. Kothari on 11/08/11.
+//  Copyright 2011 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DemoAccelerometerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DemoAccelerometerAppDelegate class]));
+	}
+}
