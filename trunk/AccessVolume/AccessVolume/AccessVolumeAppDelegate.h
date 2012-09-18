0e4e9d2715eb08cb9a0e868a7b326775ee79fb9c
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-18 12:38:47 +0000

    Accessing volume of iPhone/iPad/iPod from your iPhone Application
    Grab complete tutorial from here.
    
    http://sugartin.info/2011/08/25/accessing-volume-of-iphoneipadipod-from-your-iphone-application/

---
+++ trunk/AccessVolume/AccessVolume/AccessVolumeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AccessVolumeAppDelegate.h
+//  AccessVolume
+//
+//  Created by Sagar R. Kothari on 25/08/11.
+//  Copyright 2011 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AccessVolumeViewController;
+
+@interface AccessVolumeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) AccessVolumeViewController *viewController;
+
+@end
