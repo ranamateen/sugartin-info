ca0583ae21f40edf6ca47ffc5e8908c030cd9772
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-18 11:02:09 +0000

    MultipleCheck in Table - UITableView
    grab complete tutorial from here.
    
    http://sugartin.info/2011/08/19/multiplecheck-in-table-uitableview/

---
+++ trunk/multiCheck/multiCheck/RootViewController.h
@@ -0,0 +1,18 @@
+//
+//  RootViewController.h
+//  multiCheck
+//
+//  Created by Sagar Kothari on 08/07/11.
+//  Copyright 2011 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+@property (nonatomic, retain) NSArray *arForTable;
+@property (nonatomic, retain) NSMutableArray *arForIPs;
+
+@end
