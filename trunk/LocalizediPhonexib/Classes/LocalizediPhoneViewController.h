e2eda25acf0cd6aef4d612098dd22fe4a653e5d1
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-18 07:35:34 +0000

    Localization in Xib (Nib) Files
    See complete illustration here.
    
    http://sugartin.info/2011/08/04/localization-in-xib-nib-files/

---
+++ trunk/LocalizediPhonexib/Classes/LocalizediPhoneViewController.h
@@ -0,0 +1,16 @@
+//
+//  LocalizediPhoneViewController.h
+//  LocalizediPhone
+//
+//  Created by Sagar R. Kothari  on 09/05/09.
+//  Copyright http://sugartin.info 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalizediPhoneViewController : UIViewController {
+
+}
+
+@end
+
