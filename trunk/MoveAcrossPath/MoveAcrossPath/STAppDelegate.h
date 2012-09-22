18186867a30dad5418512fd30e97019ff1b451d8
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-22 11:27:58 +0000

    Move your views/layers across a path in XCode iOS Project.
    Grab the complete illustration from here.
    
    http://sugartin.info/2012/09/22/move-layersviews-to-path-in-ios

---
+++ trunk/MoveAcrossPath/MoveAcrossPath/STAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  STAppDelegate.h
+//  MoveAcrossPath
+//
+//  Created by sagar on 22/09/12.
+//  Copyright (c) 2012 sugartin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class STViewController;
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) STViewController *viewController;
+
+@end
