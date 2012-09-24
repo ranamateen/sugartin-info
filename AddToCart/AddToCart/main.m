a4b14e118cbab169b952adaae8c99c29cce8275a
sag333ar <sag333ar@4c9cd0c5-1662-a4a5-d177-7430fcfa8879>
2012-09-24 10:54:24 +0000

    See complete step by step illustration for exact implementation of above from following link.
    http://sugartin.info/2012/09/24/add-to-cart-shopping-cart-view-animation-using-uitabbar-uitableview

---
+++ AddToCart/AddToCart/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AddToCart
+//
+//  Created by Sagar R. Kothari on 24/09/12.
+//  Copyright (c) 2012 http://sugartin.info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([STAppDelegate class]));
+	}
+}
